4747714d8dcee2bf05119ef8154338adae66edf3
Armen G <gharibi.armen@yahoo.com>
2017-04-30 11:37:37 -0700

    added 1) loginFailurePublisher/EventHandler 2) removed SpringBoot auto configuration thymeleaf, and added spring thymeleaf config instead.

---
+++ src/main/java/com/agharibi/services/security/LoginFailureEventHandler.java
@@ -0,0 +1,16 @@
+package com.agharibi.services.security;
+
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginFailureEventHandler implements ApplicationListener<LoginFailureEvent> {
+
+    @Override
+    public void onApplicationEvent(LoginFailureEvent event) {
+        Authentication authentication = (Authentication) event.getSource();
+        System.out.println("Login failure for " + authentication.getPrincipal());
+    }
+}
